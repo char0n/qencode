6c894890ccb74032aee8b507a803530ccb488fd2
Vladimir Gorej <vladimir.gorej@gmail.com>
2019-08-02 16:52:01 +0200

    refactor: simplify tests and use mix config
    Ref #3

--- test/qencode/client_test.exs
+++ test/qencode/client_test.exs
@@ -4,11 +4,8 @@ defmodule Qencode.ClientTest do
   use ExUnit.Case
   doctest Qencode.Client
 
-  describe "new: given valid api_key" do
-    setup do
-      api_key = System.get_env("QENCODE_API_KEY")
-      %{api_key: api_key}
-    end
+  describe "given valid api_key" do
+    setup [:provide_api_key]
 
     test "should return access token", %{api_key: api_key} do
       client = Client.new!(api_key)
@@ -19,11 +16,26 @@ defmodule Qencode.ClientTest do
     end
   end
 
-  describe "get: given invalid api_key" do
+  describe "given api_key provided by mix configuration" do
+    test "should return access token" do
+      client = Client.new!()
+
+      assert %{session_token: session_token} = client
+      assert is_binary(session_token)
+      assert String.length(session_token) === 32
+    end
+  end
+
+  describe "given invalid api_key" do
     test "should raise MatchError" do
       assert_raise MatchError, fn ->
         Client.new!("invalid_api_key")
       end
     end
+  end
+
+  defp provide_api_key(_context) do
+    api_key = Application.get_env(:qencode, :api_key)
+    %{api_key: api_key}
   end
 end
